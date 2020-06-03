691d7a8c04aa100d989e14426f56b047fd9a827d
SemaoHakeem <semaoadedayo@gmail.com>
2020-06-03 17:56:25 +0100

    Hakeem-HNG-01232 script

---
+++ scripts/Hakeem-HNG-01232.js
@@ -0,0 +1,6 @@
+let myName = "Semao hakeem";
+let myId = "HNG-01232";
+let myEmail = "semaoadedayo@gmail.com";
+let myLanguage = "Javascript";
+
+console.log(`Hello world, this is ${myName} with HNGi7 ID ${myId} and email ${myEmail} using ${myLanguage} for stage 2 task`);
